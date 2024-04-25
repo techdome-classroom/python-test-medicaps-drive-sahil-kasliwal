1832218f2de7ec12afe0599c7b6f121cf2e0b265
Sahil Kasliwal <sahilkasliwal7@gmail.com>
2024-04-25 13:44:21 +0530

    Solved

--- program1.py
+++ program1.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        list = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping:
+                top_element = list.pop() if list else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                list.append(char)
+        return not list
     
 
 
--- program2.py
+++ program2.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+        for char in s:
+            value = roman_dict[char]
+            result += value
+            if value > prev_value:
+                result -= 2 * prev_value
+            prev_value = value
+        return result
+    
